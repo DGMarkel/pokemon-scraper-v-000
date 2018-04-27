493452ae7528106ca189f647c2ab1644b4295034
David Markel <davidgmarkel@gmail.com>
2018-04-27 16:19:59 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,8 +12,8 @@ attr_accessor :id, :type, :name, :db
   end
 
   def self.find(id, db)
-    pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
-    save(pokemon[0][0], pokemon[0][1], pokemon[0][2])
+    pokemon_from_db = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
+    save(pokemon_from_db[0][0], pokemon_from_db[0][1], pokemon_from_db[0][2])
   end
 
 
