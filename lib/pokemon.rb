693ecc5598569d4a44a22ae0620947b4aecbc58e
David Markel <davidgmarkel@gmail.com>
2018-04-27 16:34:56 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -13,10 +13,10 @@ attr_accessor :id, :type, :name, :db
 
   def self.find(id, db)
     pokemon_from_db = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
+    binding.pry
     pokemon_hash = {
     :name => pokemon_from_db[0][1],
     :type => pokemon_from_db[0][2],
-    binding.pry
     :db = db
     }
     Pokemon.new(pokemon_hash)
